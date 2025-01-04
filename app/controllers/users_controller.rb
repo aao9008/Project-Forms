7b7bc07976dcba3fe2811723e8fe76aade8b39ac
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-01-04 03:00:59 -0500

    Add UsersController with new and create actions
    This commit adds a UsersController with empty #new and #create methods
    to handle user creation requests.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def new
+    # Code for displaying the user form will go here
+  end
+
+  def create
+    # Code for creating a new user will go here
+  end
+end
