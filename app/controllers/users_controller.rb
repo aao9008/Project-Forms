6bac531f8d7caff2b5f4b5149b9e72540e91be07
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-01-05 17:53:04 -0500

    Change user controller's #create method
    Switched user controller’s #create method again to accept the
    nested :user hash from params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    #@user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = "Great! Your user account has been created!"
