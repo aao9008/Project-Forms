0486fb12a77cab4f85ff640c19330d54082787ef
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-01-04 19:21:33 -0500

    Add user_params method and flash messages for form handling
    - Implemented user_params method to safely handle user input.
    - Added flash messages for success and error notifications when creating a user.
    - Ensured user data persists on form re-render after validation failures.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,25 @@
 class UsersController < ApplicationController
   def new
-    # Code for displaying the user form will go here
+    @user = User.new
   end
 
   def create
-    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    @user = User.new(user_params)
 
     if @user.save
+      flash[:success] = "Great! Your user account has been created!"
       redirect_to new_user_path
     else
       # Log the validation errors
       Rails.logger.debug @user.errors.full_messages
+      flash.now[:error] = "Rats! Fix your mistakes, please."
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [ :username, :email, :password ])
   end
 end
